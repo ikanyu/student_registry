89d5ebe74c30cc363cfe8c62b252cb5cbd274a93
WenYu <wongwy93@gmail.com>
2020-12-06 11:09:03 +0800

    Refactoring commonstudent

--- api/students.js
+++ api/students.js
@@ -1,16 +1,35 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router()
 
-const Student = require("../models/Student")
+const Student = require("../models/Student");
+const Teacher = require("../models/Teacher");
 
-router.get("/students", (req, res) => {
-  // console.log("hello");
-  console.log(req.query);
-  res.send(req.query.email);
-  // User.query()
-    //     .then(users => {
-    //         res.json(users)
-    //     })
+router.get("/commonstudents", async (req, res) => {
+  let teachers;
+  let students;
+  let teachersEmail = req.query.teacher;
+
+  if (typeof teachersEmail === 'string' || teachersEmail instanceof String) {
+    teachers = await Teacher.query()
+      .where('email', teachersEmail)
+      .withGraphFetched('students')
+
+    res.json(teachers[0].students.map(student => student.email))
+  } else {
+    teacher = await Teacher.query()
+      .select('student_id')
+      .count()
+      .join('student_teacher', 'teachers.id', '=', 'student_teacher.teacher_id')
+      .whereIn('teachers.email', teachersEmail)
+      .groupBy('student_id')
+      .having('count(*)', '=', teachersEmail.length)
+
+    console.log(teachers);
+    students = await Student.query()
+      .whereIn('id', teacher.map(student => student.student_id))
+
+    res.json(students.map(student => student.email))
+  }
 })
 
 module.exports = {
